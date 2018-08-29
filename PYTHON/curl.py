91c2a9a9de67946071544905215e13a38f039c6b
wajatmaka <wajatmaka@gmail.com>
2018-08-29 11:54:00 +0700

    penambahan tools git

--- PYTHON/curl.py
+++ PYTHON/curl.py
@@ -1,28 +1,12 @@
-import requests, json
-from prettytable import PrettyTable
+import requests, json, sys
 
 
-url = 'https://bitbucket.org/api/1.0/user/'
-headers = {'Content-Type': 'application/json'}
+f = open("ada","r") 
+data=f.read()
 
-r = requests.get(url, auth=('wajatmaka', '228844@galuh'), headers=headers)
-print(r.status_code)
+hasil = json.loads(data)
 
 
-hasil = json.loads(r.text)
-print(json.dumps(hasil, indent=4, sort_keys=True))
-
-print("Print In Tables Repositories")
-
-t = PrettyTable(['CREATED ON','OWNER','LOGO','STATE'])
-
-for x in hasil['repositories']:
-    a = x['created_on']
-    b = x['owner']
-    c = x['logo']
-    d = x['state']
-    t.add_row([a,b,c,d])
-print(t)
 
 
 
